273ec108a2cfff30530a2450b9d51c14aa54905d
DannyBrito <britodanny1997@gmail.com>
2019-10-13 16:09:58 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,19 @@
 class BoatingTest
 
+    attr_accessor :student, :instructor, :test, :status
+    @@all =[]
+
+    def initialize (student, test, status, instructor)
+        @student = student
+        @instructor = instructor
+        @test = test
+        @status = status
+
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,41 @@
 class Student
 
+    attr_accessor :first_name
+    @@all =[]
+
+    def self.all
+        @@all
+    end
+
+    def initialize(first_name)
+        @first_name = first_name
+
+        self.class.all << self
+    end
+
+    def add_boating_test(test, status, instructor)
+        BoatingTest.new(self, test, status, instructor)
+    end
+
+    def self.find_student(student)
+        all.find{|student_instance| student_instance.first_name == student}
+    end
+
+    def grade_percentage
+        total = test_taken.count
+        passed = test_taken.select{|test| test.status == "passed"}.count
+        return (passed/total.to_f)*100 if total.to_f != 0
+        "Grade percentage is 100 as none test has a status of passed/failed"
+    end
+
+    #private
+
+    def tests
+        BoatingTest.all.select{|test| test.student == self}
+    end
+
+    def test_taken
+        tests.select{|test| test.status == "passed" || test.status == "failed" }
+    end
+
 end
