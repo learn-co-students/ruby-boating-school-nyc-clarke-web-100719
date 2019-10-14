6316766634acda69a050b283a269fc6fb2742286
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-14 00:32:28 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,38 @@
 class Instructor
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def boating_tests
+        BoatingTest.all.select { |boating_test| boating_test.instructor == self }
+    end
+
+    def students
+        boating_test.map { |boating_test| boating_test.student }.uniq
+    end
+
+    def pass_student(student, test_name)
+        test = find_test_by_name_and_test_name(student, test_name)
+        test ? test.status = 'passed' : test = BoatingTest.new(student, test_name, 'passed', self)
+        test
+    end
+
+    def fail_student(student, test_name)
+        test = find_test_by_name_and_test_name(student, test_name)
+        test ? test.status = 'failed' : test = BoatingTest.new(student, test_name, 'failed', self)
+        test
+    end
+
+    def find_test_by_name_and_test_name(student, test_name) #searches for student that matches student and test_name criteria
+        boating_tests.find { |boating_test| boating_test.student == student && boating_test.test_name == test_name }
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
+    attr_accessor :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, status, instructor)
+        BoatingTest.new(self, test_name, status, instructor)
+    end
+
+    def self.find_student(first_name)
+        self.all.find { |student| student.first_name == first_name }
+    end
+
+    def boating_tests
+        BoatingTest.all.select { |boating_test| boating_test.student == self }
+    end
+
+    def grade_percentage
+        num_passed_tests = boating_tests.reduce(0) do |passed_tests, boating_test|
+            boating_test.status == 'passed' ? passed_tests + 1 : passed_tests
+        end
+        num_passed_tests / boating_tests.count.to_f * 100
+    end
 
 end
